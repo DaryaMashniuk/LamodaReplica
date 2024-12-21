007a9c9edad079a995fa31d7c3bfde9a8882aa8b
Darya <darya.m.2006@mail.ru>
2024-12-21 20:54:28 +0300

    Change: file location

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -15,18 +15,18 @@ export const CATEGORIES = [
 export const MAXPRICE = 9999;
 export const MINPRICE =10;
 export const IMAGES= [
-    "src/assets/images/1.jpg",
-    "src/assets/images/2.jpg",
-    "src/assets/images/3.jpg",
-    "src/assets/images/4.jpg",
-    "src/assets/images/5.jpg",
-    "src/assets/images/6.jpg",
-    "src/assets/images/7.jpg",
-    "src/assets/images/8.jpg",
-    "src/assets/images/9.jpg",
-    "src/assets/images/10.jpg",
-    "src/assets/images/11.jpg",
-    "src/assets/images/12.jpg",
+    "/assets/images/1.jpg",
+    "/assets/images/2.jpg",
+    "/assets/images/3.jpg",
+    "/assets/images/4.jpg",
+    "/assets/images/5.jpg",
+    "/assets/images/6.jpg",
+    "/assets/images/7.jpg",
+    "/assets/images/8.jpg",
+    "/assets/images/9.jpg",
+    "/assets/images/10.jpg",
+    "/assets/images/11.jpg",
+    "/assets/images/12.jpg",
 ]
 
 export const ITEMS_PER_PAGE = 20;
